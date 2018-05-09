665fd0cfa6c1ffd3a3f4598413ee4516277d4663
Listen.Li <listioning@163.com>
2018-05-09 14:16:21 +0800

    Update zmem.h

--- zmem.h
+++ zmem.h
@@ -1,10 +1,11 @@
-#ifndef __ZMALLOC_H
-#define __ZMALLOC_H
+#ifndef __ZMEM_H
+#define __ZMEM_H
 
 
+unsigned int zusedsize();
+void zclear();
 void *zmalloc(size_t size);
 void zfree(void *ptr);
-void zclear();
 
 
 #endif
