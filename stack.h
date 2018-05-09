e9209a54b9a9724b8863ec066725ecc7ef3d43e3
Listen.Li <listioning@163.com>
2018-05-09 14:39:20 +0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,37 +1,18 @@
-//////////////////////////////////////////////////////////////////////////
-///  Stack Impl
-/* 
-The top index is defined -100, we i meet a Index as the top(-100), 
-I will send the topest Item
-*/
 #ifndef CMMN_STACK_H
 #define CMMN_STACK_H
 
+
 #define MR_STACK_TOP_INDEX          (-100)
 
-INT32
-MR_StackCreate(TN_HANDLE *phStack);
 
-BOOL
-MR_StackIsEmpty(void *hStack);
-
-INT32
-MR_StackGetAt(void *hStack, INT32 iIndex, void **ppvItem);
-
-INT32
-MR_StackPush(void *hStack, void *pvItem, INT32 *piIndex);
-
-void*
-MR_StackPopup(void *hStack);
-
-INT32
-MR_StackDelete(void *hStack);
-
-INT32
-MR_StackClean(void *hStack);
-
-INT32
-MR_StackGetSize(void *hStack);
+int MR_StackCreate(void *phStack);
+bool MR_StackIsEmpty(void *hStack);
+void* MR_StackGetAt(void *hStack, int iIndex);
+int MR_StackPush(void *hStack, void *pvItem, int *piIndex);
+void* MR_StackPopup(void *hStack);
+int MR_StackDelete(void *hStack);
+int MR_StackClean(void *hStack);
+int MR_StackSize(void *hStack);
 
 
 #endif
